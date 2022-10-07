43c0f37dfa623f5a526cd1c3c83b4142ee52bc7c
Climson Jr <jrbuking@gmail.com>
2022-10-07 19:18:25 +1300

    Second Commit

---
+++ src/App.js
@@ -0,0 +1,182 @@
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
+
+// Axios is a library which allows you to make requests to an API
+
+function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  const [newWage, setNewWage] = useState(0);
+
+  // Show stat employees
+  const [employeeList, setEmployeeList] = useState([]);
+
+  const addEmployee = () => {
+    // Send info from FE TO BE - Basically sending objects below to BE
+    Axios.post("https://jun-crud-react-deploy.herokuapp.com/create", {
+      //LOCAL - http://localhost:3001/create
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
+      console.log("success");
+      setEmployeeList([
+        ...employeeList,
+        {
+          name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,
+        },
+      ]);
+    });
+  };
+
+  const getEmployees = () => {
+    Axios.get("https://jun-crud-react-deploy.herokuapp.com/employees").then(
+      (response) => {
+        setEmployeeList(response.data);
+      }
+    );
+  };
+
+  // Update employees
+  const updateEmployeeWage = (id) => {
+    Axios.put("https://jun-crud-react-deploy.herokuapp.com/update", {
+      wage: newWage,
+      id: id,
+    }).then((response) => {
+      // alert("Update");
+      // Shows the new changes immediately
+      setEmployeeList(
+        employeeList.map((val) => {
+          return val.id == id
+            ? {
+                id: val.id,
+                name: val.name,
+                country: val.country,
+                position: val.position,
+                age: val.age,
+                wage: newWage,
+              }
+            : val;
+        })
+      );
+    });
+  };
+
+  // Delete employee
+  const deleteEmployee = (id) => {
+    Axios.delete(
+      `https://jun-crud-react-deploy.herokuapp.com/delete/${id}`
+    ).then((response) => [
+      setEmployeeList(
+        employeeList.filter((val) => {
+          return val.id != id;
+        })
+      ),
+    ]);
+  };
+
+  return (
+    <div className="App">
+      <div className="information">
+        <label>Name:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        ></input>
+        <label>Age:</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        ></input>
+        <label>Country:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        ></input>
+        <label>Position:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        ></input>
+        <label>Wage (year):</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        ></input>
+        <button onClick={addEmployee}>Add Employee</button>
+      </div>
+
+      {/* Show eomplyees */}
+      <div className="employees">
+        <button onClick={getEmployees} className="employeeBtn">
+          Show Employees
+        </button>
+
+        {employeeList.map((val, key) => {
+          return (
+            <div className="employee">
+              <div>
+                <h3>Name: {val.name}</h3>
+                <h3>Age: {val.age}</h3>
+                <h3>Country: {val.country}</h3>
+                <h3>Position:{val.position}</h3>
+                <h3>Wage: {val.wage}</h3>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="2000.."
+                  onChange={(event) => {
+                    setNewWage(event.target.value);
+                  }}
+                ></input>
+
+                {/* Update button */}
+                <button
+                  onClick={() => {
+                    updateEmployeeWage(val.id);
+                  }}
+                  className="employeeBtn"
+                >
+                  Update
+                </button>
+
+                {/* Delete Button */}
+                <button
+                  onClick={() => {
+                    deleteEmployee(val.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
